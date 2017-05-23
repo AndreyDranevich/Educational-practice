9e9893c0c604adb02197554dfe241e68fe39704a
Andrew <andreydran7@gmail.com>
2017-05-23 10:35:11 +0300

    Add 9 task Clean code

--- app.js
+++ app.js
@@ -1,35 +1,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const db = require('diskdb');
+
 const app = express();
 db.connect('public/db', ['articles']);
 
 app.use(express.static('public/UI'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const port = 3000;
-app.listen(port, () => {
-    console.log('We are live on ' + port);
+app.listen(3000, () => {
+  console.log('We are live on 3000');
+});
+app.get('/articles', (req, res) => {
+  res.json(db.articles.find());
 });
 
-app.get('/articles', function (req, res) {
-    res.json(db.articles.find());
+app.get('/article/:id', (req, res) => {
+  res.json(db.articles.findOne({ id: req.params.id }));
 });
 
-app.get('/article/:id', function (req, res) {
-    res.json(db.articles.findOne({id: req.params.id}));
+app.post('/article', (req, res) => {
+  res.json(db.articles.save(req.body));
 });
 
-app.post('/article', function (req, res) {
-    res.json(db.articles.save(req.body));
+app.delete('/articles/:id', (req, res) => {
+  res.json(db.articles.remove({ id: req.params.id }));
 });
 
-app.delete('/articles/:id', function (req, res) {
-    +res.json(db.articles.remove({id: req.params.id}));
-});
-
-app.put('/articles/', function (req, res) {
-    +res.json(db.articles.update({id: req.body.id}, req.body));
+app.put('/articles/', (req, res) => {
+  res.json(db.articles.update({ id: req.body.id }, req.body));
 });
 
--- public/UI/js/article_routes.js
+++ public/UI/js/article_routes.js
@@ -1,60 +1,60 @@
-let article_routes = (function () {
-    const req = new XMLHttpRequest();
+const articleRoutes = (function get() {
+  const req = new XMLHttpRequest();
 
-    function getArticle(id) {
-        req.open('GET', '/article/' + id, false);
-        req.setRequestHeader('content-type', 'application/json');
-        req.send();
+  function getArticle(id) {
+    req.open('GET', '/article/' + id, false);
+    req.setRequestHeader('content-type', 'application/json');
+    req.send();
 
-        const article = JSON.parse(req.responseText);
-        article.createdAt = new Date(article.createdAt);
-        return article;
-    }
+    const article = JSON.parse(req.responseText);
+    article.createdAt = new Date(article.createdAt);
+    return article;
+  }
 
-    function getArticles() {
-        req.open('GET', '/articles', false);
-        req.setRequestHeader('content-type', 'application/json');
-        req.send();
-        const articles = JSON.parse(req.responseText);
-        articles.forEach((currentArticle) => {
-            currentArticle.createdAt = new Date(currentArticle.createdAt);
-        });
-        return articles;
-    }
+  function getArticles() {
+    req.open('GET', '/articles', false);
+    req.setRequestHeader('content-type', 'application/json');
+    req.send();
+    const articles = JSON.parse(req.responseText);
+    articles.forEach((currentArticle) => {
+      currentArticle.createdAt = new Date(currentArticle.createdAt);
+    });
+    return articles;
+  }
 
-    function editArticle(id, article) {
-        req.open('PUT', '/article/' + id, false);
-        req.setRequestHeader('content-type', 'application/json');
-        req.onerror = function () {
-            reject(new Error("Error"));
-        };
-        req.send(JSON.stringify(article));
+  function editArticle(id, article) {
+    req.open('PUT', '/article/' + id, false);
+    req.setRequestHeader('content-type', 'application/json');
+    req.onerror = function () {
+      reject(new Error('Error'));
+    };
+    req.send(JSON.stringify(article));
 
-    }
+  }
 
-    function addArticle(article) {
-        req.open('POST', '/article', false);
-        req.setRequestHeader('content-type', 'application/json');
-        req.onerror = function () {
-            reject(new Error("Error"));
-        };
-        req.send(JSON.stringify(article));
-    }
+  function addArticle(article) {
+    req.open('POST', '/article', false);
+    req.setRequestHeader('content-type', 'application/json');
+    req.onerror = function () {
+      reject(new Error('Error'));
+    };
+    req.send(JSON.stringify(article));
+  }
 
-    function deleteArticle(id) {
-        req.open('DELETE', '/articles/' + id, false);
-        req.setRequestHeader('content-type', 'application/json');
-        req.onerror = function () {
-            reject(new Error("Erroxr"));
-        };
-        req.send();
-    }
+  function deleteArticle(id) {
+    req.open('DELETE', '/articles/' + id, false);
+    req.setRequestHeader('content-type', 'application/json');
+    req.onerror = function () {
+      reject(new Error('Error'));
+    };
+    req.send();
+  }
 
-    return {
-        getArticle: getArticle,
-        getArticles: getArticles,
-        deleteArticle: deleteArticle,
-        addArticle: addArticle,
-        editArticle: editArticle
-    }
+  return {
+    getArticle,
+    getArticles,
+    deleteArticle,
+    addArticle,
+    editArticle,
+  };
 }());
